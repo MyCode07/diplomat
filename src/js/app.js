9d81a80379b7ca93dd2748fbf742930a708d3680
MyCode07 <erem19992015@mail.ru>
2025-08-11 09:00:04 +0300

    first log

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+// import { maskInputs } from "./static/inputmask.js";
+// import "./parts/menu.js";
+// import "./parts/popup.js";
+// import "./parts/sliders.js";
+// import "./parts/show-more.js";
+// import { accorden } from "./static/accordeon.js";
+// import { Fancybox } from "@fancyapps/ui";
+// import { stickyHeader } from "./parts/header.js";
+
+// stickyHeader();
+// accorden();
+
+// maskInputs('+7 (999) 999-99-99', '._mask-phone')
+
+// Fancybox.bind("[data-fancybox]", {
+// });
+
+document.addEventListener('click', function (e) {
+    let targetEl = e.target;
+    if (targetEl.classList.contains('pages-close')) {
+        document.querySelector('.pages').classList.toggle('_hide');
+    }
+})
